60563c15661a8b4a7a6d00783bf199f781c6cf5b
Eren Kaplan <erenkaplan@tutanota.com>
2025-09-19 22:16:19 +0300

    i3 & polybar changes

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -4,9 +4,20 @@
 polybar-msg cmd quit
 # killall -q polybar
 
-# Launch Polybar bars for dual monitors
-polybar sag 2>&1 | tee -a /tmp/polybar-sag.log & disown
-polybar sol 2>&1 | tee -a /tmp/polybar-sol.log & disown
+# Check connected monitors
+monitors=$(xrandr --query | grep " connected" | cut -d ' ' -f1)
 
-echo "Polybar launched..."
+# Launch Polybar based on connected monitors
+if echo "$monitors" | grep -q "eDP-1"; then
+    polybar macbook 2>&1 | tee -a /tmp/polybar-macbook.log & disown
+fi
 
+if echo "$monitors" | grep -q "HDMI-0"; then
+    polybar sag 2>&1 | tee -a /tmp/polybar-sag.log & disown
+fi
+
+if echo "$monitors" | grep -q "DP-0"; then
+    polybar sol 2>&1 | tee -a /tmp/polybar-sol.log & disown
+fi
+
+echo "Polybar launched based on connected monitors..."
--- dot_config/polybar/monitors.sh
+++ dot_config/polybar/monitors.sh
@@ -2,6 +2,10 @@
 
 monitors=$(xrandr --query | grep " connected" | cut -d ' ' -f1)
 
+if [[ "$monitors" == "eDP-1" ]]; then
+    polybar sol &  
+fi
+
 if echo "$monitors" | grep -q "HDMI-0"; then
     polybar sag &
 fi
@@ -10,7 +14,4 @@ if echo "$monitors" | grep -q "DP-0"; then
     polybar sol &
 fi
 
-if [[ "$monitors" == "eDP-1" ]]; then
-    polybar sol &  
-fi
 
